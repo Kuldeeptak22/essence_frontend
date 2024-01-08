2aabb41dc0781873db5102061c0347ee3bc4dd54
Kuldeep tak <Kuldeeptak2211@gmail.com>
2024-01-08 16:56:20 +0530

    initial Commit : react-router setup, tailwind, mui library,sass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AppBar from "./components/AppBar/AppBar";
+import HomePage from "./pages/HomePage";
+import NewPage from "./pages/NewPage";
+import AboutPage from "./pages/AboutPage";
+import OffersPage from "./pages/OffersPage";
+import { ContactPage } from "@mui/icons-material";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <AppBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/newPage" element={<NewPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/offers" element={<OffersPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </Router>
+      <h1>hello</h1>
     </div>
   );
 }

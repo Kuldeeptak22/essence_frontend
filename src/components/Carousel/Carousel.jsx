f081d26469efca5afacb4bed112d6565bc8a3bf2
Kuldeep tak <Kuldeeptak2211@gmail.com>
2024-01-10 16:40:45 +0530

    carousel added

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Container, Row } from "react-bootstrap";
+import "./Carousel.scss";
+
+const Carousel = () => {
+  const settings = {
+    // dots: true,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    lazyLoad: "ondemand",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <Container
+      fluid
+      className="d-flex justify-center flex-col align-middle items-center g-0"
+    >
+      <Row className="w-[100%] p-0">
+        {/* {isLoading && (
+          <div className="d-flex">
+            <Skeleton
+              variant="rectangle"
+              animation="wave"
+              height={220}
+              width={400}
+            />
+          </div>
+        )} */}
+
+        <Slider {...settings} style={{padding:0}}>
+          <div className="caraoselSlider imagesSlides1"></div>
+          <div className="caraoselSlider imagesSlides2"></div>
+          <div className="caraoselSlider imagesSlides3"></div>
+          <div className="caraoselSlider imagesSlides4"></div>
+          <div className="caraoselSlider imagesSlides5"></div>
+          <div className="caraoselSlider imagesSlides6"></div>
+        </Slider>
+      </Row>
+    </Container>
+  );
+};
+
+export default Carousel;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import CategoryBar from "../components/Category/CategoryBar";
+import Carousel from "../components/Carousel/Carousel";
 
 const HomePage = () => {
   return (
@@ -8,6 +9,11 @@ const HomePage = () => {
       <Row className="g-0">
         <Col>
           <CategoryBar />
+        </Col>
+      </Row>
+      <Row className="g-0">
+        <Col>
+          <Carousel />
         </Col>
       </Row>
     </Container>
